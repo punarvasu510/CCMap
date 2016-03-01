ac3794af8a66657d83584b046e3d90de53fa0e32
punarvasu510 <punarvasu510@gmail.com>
2016-03-02 00:02:02 +0530

    modified code- includes input from user

--- dbtrial.py
+++ dbtrial.py
@@ -2,7 +2,10 @@
 import urllib2
 import json
 
-j = urllib2.urlopen('http://restcountries.eu/rest/v1/region/africa')
+
+continent = raw_input("Enter continent: ")
+
+j = urllib2.urlopen('http://restcountries.eu/rest/v1/region/' + continent)
 j_obj = json.load(j)
 length= len(j_obj)
 for country in j_obj:
